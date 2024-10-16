05f0aad1708b85d487dea1d26b600cedcaea3a8e
ALWANROFIQI <alwanrofiqi87@gmail.com>
2024-10-16 22:08:19 +0700

    tipis lagi

--- script.js
+++ script.js
@@ -21,6 +21,18 @@ $(".btn-signIn").hover(
     }
   );
 
+  //Button Daftar sekarang
+$(".btn-daftar").hover(
+  function() {
+    $(this).css('background-color', '#DFF5FF');
+    $(this).css('color', '#213555'); // saat mouse masuk
+  },
+  function() {
+    $(this).css('background-color', '#f5efe7');
+    $(this).css('color', '#213555'); // saat mouse keluar (reset ke warna asli)
+  }
+);
+
 //animasi shake diskon
   $(document).ready(function(){
     function shake() {
